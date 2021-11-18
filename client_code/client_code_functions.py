85f7a41ed4fd3294a20616081e9e54b66d1bb481
ben <benlawraus@gmail.com>
2021-11-18 03:44:11 -0800

    added more substantial test to @anvil.server.callable

--- client_code/client_code_functions.py
+++ client_code/client_code_functions.py
@@ -1,7 +1,33 @@
 """Used for pyDALAnvilWorks tests only. Delete."""
 import anvil.server
+from datetime import datetime
 
 
 def example_A():
     text = anvil.server.call("example_1")
     return text
+
+
+def generate_contact(user=None):
+    right_now = datetime.now().replace(microsecond=0)
+    phone_dict = dict(number="(510) 666-8888",
+                      created_by=user,
+                      created_on=right_now)
+    email_dict = dict(address="rex@exopotamia.com",
+                      created_by=user,
+                      created_on=right_now)
+    contact_dict = dict(
+        name="Rex Eagle",
+        phone=phone_dict,
+        email_list=[email_dict],
+        age=6,
+        created_by=user,
+        created_on=right_now)
+    return contact_dict
+
+
+def save_contact():
+    user = anvil.users.get_user()
+    contact_dict = generate_contact(user)
+    contact_dict['id'] = anvil.server.call("save_contact", contact_dict)
+    return user, contact_dict['id']
--- server_code/server_code_functions.py
+++ server_code/server_code_functions.py
@@ -1,7 +1,18 @@
 """Used for pyDALAnvilWorks tests only. Delete."""
 import anvil.server
+from anvil.tables import app_tables
 
 
 @anvil.server.callable
 def example_1() -> str:
     return "Returned string from function example_1."
+
+
+@anvil.server.callable
+def save_contact(contact_dict):
+    phone_row = app_tables.phone.add_row(**contact_dict['phone'])
+    email_row = app_tables.email.add_row(**contact_dict['email_list'][0])
+    contact_dict['phone'] = phone_row
+    contact_dict['email_list'] = [email_row]
+    contact_row = app_tables.contact.add_row(**contact_dict)
+    return contact_row.get_id()
