f29087ebae48b44e8fc33ad975d2d1bc305fc9ef
ben <benlawraus@gmail.com>
2022-05-08 05:10:54 -0700

    message for self.item

--- _anvil_designer/common_structures.py
+++ _anvil_designer/common_structures.py
@@ -6,5 +6,7 @@ class ClassDict:
         self.__dict__[key] = value
 
     def __getitem__(self, item):
+        if item not in self.__dict__.keys():
+            return "Unassigned. (Databindings not implemented yet.)"
         return self.__dict__[item]
 
