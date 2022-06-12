8edc67b0fbe65bdcc0ef6fd2424f55046cacba7c
ben <benlawraus@gmail.com>
2022-06-11 19:41:24 -0700

    added some stubs in order to use popover in anvil_extras

--- anvil/__init__.py
+++ anvil/__init__.py
@@ -5,25 +5,29 @@ import pydal.helpers.classes, pydal.objects
 from .anvil_ui import *
 from .component import *
 
+
 class dict(UserDict):
     def __init__(self, *args, **kwargs):
         if len(args) > 0:
             if isinstance(args[0], pydal.helpers.classes.Reference):
-                args_ix = self.loop_attr(args[0]._table._fields,args)
+                args_ix = self.loop_attr(args[0]._table._fields, args)
                 super().__init__(args_ix)
                 return
-            elif isinstance(args[0],pydal.objects.Row):
-                args_ix = self.loop_attr(args[0].__dict__.keys(),args)
+            elif isinstance(args[0], pydal.objects.Row):
+                args_ix = self.loop_attr(args[0].__dict__.keys(), args)
                 super().__init__(args_ix)
                 return
         super().__init__(*args, **kwargs)
         return
 
     def loop_attr(self, attr_list, args):
-        args_ix=[]
+        args_ix = []
         for attr in attr_list:
-            if attr in ('id','update_record','delete_record'):
+            if attr in ('id', 'update_record', 'delete_record'):
                 continue
             val = args[0][attr]
             args_ix.append((attr, val))
         return args_ix
+
+
+app = Mock()
--- anvil/js/window/__init__.py
+++ anvil/js/window/__init__.py
@@ -1,2 +1,23 @@
-def Function(*args,**kwargs):
-    pass
+from unittest.mock import Mock
+from dataclasses import dataclass, field
+
+
+def Function(*args, **kwargs):
+    return Mock()
+
+
+Promise = Mock()
+
+document = Mock()
+document.body = Mock()
+
+jQuery = Mock()
+jQuery.appendTo = Mock()
+
+
+def addEventListener(*args, **kwargs):
+    return Mock()
+
+
+def removeEventListener(*args, **kwargs):
+    return Mock()
